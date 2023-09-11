6131d0aebe8216e97817cad1bd783429ffdec469
ERokhmanko <rizhii_mishonok@mail.ru>
2023-09-11 18:15:25 +0300

    first commit

---
+++ app/src/main/java/ru/netology/rickandmorty/ui/EpisodeFragment.kt
@@ -0,0 +1,58 @@
+package ru.netology.rickandmorty.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.netology.rickandmorty.R
+import ru.netology.rickandmorty.databinding.FragmentEpisodesBinding
+
+class EpisodeFragment : Fragment() {
+    private var _binding: FragmentEpisodesBinding? = null
+    private val binding get() = _binding!!
+
+    @Deprecated(
+        "Deprecated in Java", ReplaceWith(
+            "inflater.inflate(R.menu.menu_list_object, menu)",
+            "ru.netology.rickandmorty.R"
+        )
+    )
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_list_object, menu)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.search -> {
+                //TODO
+                return true
+            }
+            R.id.filter -> {
+                //TODO
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEpisodesBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
